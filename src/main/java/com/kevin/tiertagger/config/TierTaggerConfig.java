581d90bc2f0db7c61d0c37b1235a0459580029fc
dexter-forum <editssigma859@gmail.com>
2024-07-10 10:45:32 -0400

    Update TierTaggerConfig.java

--- src/main/java/com/kevin/tiertagger/config/TierTaggerConfig.java
+++ src/main/java/com/kevin/tiertagger/config/TierTaggerConfig.java
@@ -6,6 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import net.uku3lig.ukulib.config.IConfig;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -21,5 +26,31 @@ public class TierTaggerConfig implements IConfig<TierTaggerConfig> {
     @Override
     public TierTaggerConfig defaultConfig() {
         return new TierTaggerConfig();
+    }
+
+    public void testApiConnection() {
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "CustomUserAgent/1.0");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            in.close();
+            connection.disconnect();
+
+            System.out.println("Response Content: " + content.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
